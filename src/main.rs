1ab0f767260905ad2f1d8f1090771e83d3868d9a
Nicki Dorrmann <nicki.dorrmann@googlemail.com>
2022-03-17 15:06:20 +0100

    basic Hello World example in actix implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, HttpResponse, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(hello)
+    }).bind(("127.0.0.1", 8081))?.run().await
 }
