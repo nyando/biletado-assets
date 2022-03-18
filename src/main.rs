8c548ee313a7c211c1832f042612c11a655ce96b
Nicki Dorrmann <nicki.dorrmann@googlemail.com>
2022-03-18 10:34:28 +0100

    manually tested basic api services

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-#[macro_use]
-extern crate diesel;
 mod db;
 
+#[macro_use]
+extern crate diesel;
+
 mod api;
-use crate::api::assets_api::get_all_buildings;
+
+use crate::api::assets_api::*;
 use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(get_all_buildings)
+        App::new()
+            .service(get_all_buildings)
+            .service(add_building)
+            .service(get_building_by_id)
     }).bind(("127.0.0.1", 8081))?.run().await
 }
