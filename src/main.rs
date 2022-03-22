3ab1de0a6d48b771d5d02d2d03f9f03c36b1964f
Nicki Dorrmann <nicki.dorrmann@googlemail.com>
2022-03-22 21:34:18 +0100

    added tracing middleware

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,16 @@ extern crate diesel;
 mod api;
 
 use dotenv::dotenv;
+use env_logger::Env;
+use log::info;
+
 use crate::db::dbconn;
 use crate::api::buildings_api::*;
 use crate::api::rooms_api::*;
 use crate::api::storeys_api::*;
-use actix_web::{App, HttpServer};
-use log::info;
+
+use actix_web::{middleware::Logger, middleware::DefaultHeaders, App, HttpServer};
+use actix_web_opentelemetry::RequestTracing;
 
 use std::io::{ErrorKind, Error};
 
@@ -22,7 +26,7 @@ async fn main() -> std::io::Result<()> {
 
     info!("initializing logging...");
     dotenv().ok();
-    env_logger::init();
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     
     info!("attempting to connect to database service...");
     if dbconn::init().is_err() { return Err(Error::new(ErrorKind::Other, "could not connect to DB service")); }
@@ -31,6 +35,10 @@ async fn main() -> std::io::Result<()> {
     info!("starting API service");
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .wrap(RequestTracing::new())
+            .wrap(DefaultHeaders::new().add(("Content-Type", "application/json")))
             .service(get_all_buildings)
             .service(add_building)
             .service(get_building_by_id)
@@ -47,5 +55,4 @@ async fn main() -> std::io::Result<()> {
             .service(update_room)
             .service(delete_room)
     }).bind(("0.0.0.0", 9000))?.run().await
-
 }
